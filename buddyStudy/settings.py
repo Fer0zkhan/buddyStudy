31a29ec2cbd9ea6a87060f4aa50bfe91f6a4a64c
Feroz Khan <ferozkhan12123@outlook.com>
2021-10-13 11:58:44 -0700

    Initial

--- buddyStudy/settings.py
+++ buddyStudy/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+MEDIA_URL = '/static/images/'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
